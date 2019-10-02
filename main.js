9eb03776fcb0c349a30bec024b2702560d128d6c
Maxime <maxime.lombardo@gmail.com>
2019-10-02 10:51:44 +0200

    azerty

---
+++ main.js
@@ -0,0 +1,6 @@
+const chalk = require("chalk");
+
+console.log(chalk.blue.bgRed("Milan"));
+console.log(chalk.red.bgGreen("Pierre"));
+console.log(chalk.yellow.bgWhite("Corinne"));
+console.log(chalk.green.bgCyanBright("Paul"));
